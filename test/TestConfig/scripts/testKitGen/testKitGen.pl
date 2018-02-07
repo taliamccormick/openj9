0f30f285dec3958fd3fbc191906f9cd820fa8d72
Talia McCormick <Talia.McCormick@ibm.com>
2018-02-07 15:32:36 -0500

    Attempt1 to fix experimental.xml generation

--- test/TestConfig/scripts/testKitGen/testKitGen.pl
+++ test/TestConfig/scripts/testKitGen/testKitGen.pl
@@ -40,7 +40,7 @@ my $impl           = '';
 my $output         = '';
 my @allLevels      = ( "sanity", "extended" );
 my @allGroups      = ( "functional", "openjdk", "external", "perf", "jck", "system" );
-my @allSubsets     = ( "SE80", "SE90", "SE100", "Panama", "Valhalla", "Experimental" );
+my @allSubsets     = ( "SE80", "SE90", "SE100", "Experimental", "Panama", "Valhalla" );
 my @allImpls       = ( "openj9", "hotspot", "sap" );
 
 foreach my $argv (@ARGV) {
